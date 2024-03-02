e86999de5e10439b658ce58b8a7e1ac41c957fad
Anushkasiyambalapitiya <anushkasiyambalapitiya.edu01@gmail.com>
2024-03-03 00:47:46 +0530

    configured tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,19 @@ import Home from './Home';
 import Login from './Login';
 import Registration from './Registration';
 import AdminDashboard from './AdminDashboard';
+//import Navbar from './Componenets/Navbar';
 
 
 function App() {
   return (
     <BrowserRouter>
          <Routes>
-          <Route path="/" element={<Home/>}></Route>
+          <Route path="/home" element={<Home/>}></Route>
           <Route path="/login" element={<Login/>}></Route>
           <Route path="/sign-up" element={<Registration/>}></Route>
           <Route path="/admin" element={<AdminDashboard/>}></Route>
-
-
-
-      
-         </Routes>
-    
-    
+   </Routes>
+  
     </BrowserRouter>
   );
 }
