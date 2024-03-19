75d7c385b6ea6614a8d11b07b2544080d06cca60
Anushkasiyambalapitiya <anushkasiyambalapitiya.edu01@gmail.com>
2024-03-20 01:25:14 +0530

    user login sign up ,reset password, genarate report

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from "dotenv";
 import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import  UserRouter  from './Routes/User.js';  
 
 dotenv.config();
 
@@ -8,6 +11,16 @@ const app = express();
 
 // middllewares
 app.use(express.json());
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+        credentials: true,
+        methods:["GET", "POST", "PUT","DELETE"],
+    }
+));
+app.use(cookieParser())
+
+app.use('/auth',UserRouter);
 
 // initialize port 
 const port = process.env.PORT ||8080;
--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -6,6 +6,9 @@ import './App.css'
 import Home from './Pages/Home'
 import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
+import ForgotPassword from './Pages/ForgotPassword'
+import Resetpassword from './Pages/Resetpassword'
+import AdminDashboard from './Pages/AdminDashboard'
 
 function App() {
  
@@ -16,6 +19,10 @@ function App() {
       <Route path='/home' element={<Home/>}></Route>
       <Route path='/login' element={<Signin/>}></Route>
       <Route path='/Signup' element={<Signup/>}></Route>
+      <Route path='/forgotpassword' element={<ForgotPassword/>}></Route>
+      <Route path='/resetpassword/:token' element={<Resetpassword/>}></Route>
+      <Route path='/admindashboard' element={<AdminDashboard/>}></Route>
+
     </Routes>
     
     </BrowserRouter>
